c7975080b2b88213e6386e1c335a6f8fb62e1089
marijo252 <maria.rojasmedrano@gmail.com>
2019-02-04 17:50:58 +0100

    creazione del transactionManager

--- Esercizi.Net/Esercizi.Net.BusinessLogic/ITransactionFactory.cs
+++ Esercizi.Net/Esercizi.Net.BusinessLogic/ITransactionFactory.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Esercizi.Net.BusinessLogic
 {
-    public interface ITransactionFactory : IFactory<ITransazione>
+    public interface ITransactionFactory : IFactory<ITransazione> //ITransactionFactory implementa una interfaccia IFactory con "tipo" Itransazione
     {
     }
 }
---
+++ Esercizi.Net/Esercizi.Net.BusinessLogic/TransactionManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizi.Net.BusinessLogic
+{
+    public class TransactionManager
+    {
+        //
+
+        public ITransactionFactory Factory{ get;}
+
+        public List<ITransazione> Transazioni { get; } // quando chiamo il get leggo il oggetto cioè ho l'accesso al oggeto (la lista)
+
+        public TransactionManager(ITransactionFactory factory) //costruttore perche ha lo stesso nome della classe
+        {
+            Factory = factory;
+            Transazioni = new List<ITransazione>();
+        }
+
+        public ITransazione CreateTransaction() 
+        //questo e un metodo che non prende nulla in input, restituisce un itransaction. 
+        //la implementazione consiste nel chiamare il metodo create della property factory di tipo itransactionfactory
+        {
+            return Factory.Create();
+        }
+        public void SaveTransaction (ITransazione transazione)
+        {
+            Transazioni.Add(transazione);
+        }
+        public ITransazione DeleteTransaction(int index)
+        {
+            return null;
+        }
+
+        public IEnumerable<ITransazione> GetTransactions()
+        {
+            return null;
+        }
+    }
+}
